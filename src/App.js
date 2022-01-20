ea204919287ba1d69ebbfc163e76aa4f3878dcb2
Gourishankar Bhutra <Gourishankar.Bhutra@onlineseminar.nl>
2022-01-20 20:22:40 +0100

    feat: fetch data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { LangSelector } from './LangSelector';
 import { useTranslation } from 'react-i18next';
 
 function App() {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+
+  useEffect(() => {
+    fetch('assets/lng.json', { headers: {
+      'Content-type': 'application/json',
+      'Accept': 'application/json'
+    }}).then((res) => res.json()).then((data) => i18n.changeLanguage(data.lng));
+  }, [i18n])
+
   return (
     <div className="App">
       <header className="App-header">
--- src/i18n.js
+++ src/i18n.js
@@ -1,11 +1,11 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-http-backend';
+import HttpBackend from 'i18next-http-backend';
 import Languagedetector from 'i18next-browser-languagedetector';
 
 const fallbackLng = ['en'];
 
-i18n.use(Backend).use(Languagedetector).use(initReactI18next).init({
+i18n.use(HttpBackend).use(Languagedetector).use(initReactI18next).init({
     lng: 'nl',
     fallbackLng,
     detection: {
